afbc18dffec04d11ebf8070de9bebd55abd53431
Suhail Ahmed <ahmed.suhail42@gmail.com>
2026-08-10 22:41:09 +0530

    merge 1

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
-const importRoutes = require("./routes/importRoutes");
-
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-
-app.use("/api/import", importRoutes);
 
 app.get("/", (req, res) => {
     res.json({
-        message: "StreamWeaver API is running"
+        message: "StreamWeaver backend is running"
     });
 });
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 });
